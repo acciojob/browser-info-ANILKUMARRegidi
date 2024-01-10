41f0e9aafb08d85163e00afb647a99e12422dd38
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2024-01-10 13:06:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+	const info = getbrowserInfo();
+	const ele = document.getElementById("browser-info");
+	ele.textContent = `You are using  ${info.name}  version  ${info.version}`;
+});
+function getbrowserInfo()
+{
+	const user_info = navigator.userAgent;
+	const browser = navigator.appName;
+	const versionNo = navigator.appVersion;
+
+	return{
+		name:browser,
+		version:versionNo
+	};
+}
